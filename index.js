ac6756133b31c0cd3bb8ee3b72746dbf22f32e6d
prmdev <93345906+prmdev@users.noreply.github.com>
2017-02-14 09:14:08 -0500

    Finished the basic functionality of the bot

--- index.js
+++ index.js
@@ -17,61 +17,109 @@ const twitter = Promise.promisifyAll(new twitterApi({
     consumerSecret: process.env.CONSUMER_TOKEN_SECRET
 }));
 
-let baseBeaverImage = null;
-function loadValentine(name) {
+function loadValentine(state) {
+    console.log('Loading in valentine image file...');
     return jimp.read('beaver.jpg')
-        .then(function (beaver) {
-            baseBeaverImage = beaver.clone();
+        .then((beaver) => {
+            console.log('Successfully loaded image');
+            state.baseBeaverImage = beaver.clone();
+            return state;
         })
-        .catch(function (error) {
-            console.error(error);
-            process.exit(1);
-        });
+        .catch(error => Promise.reject(error));
 }
 
-function loginToTwitter() {
+function loginToTwitter(state) {
+    console.log('Logging in to Twitter...');
     return twitter.verifyCredentialsAsync(process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET, null)
-        .then(function (data, response) {
-            console.log(data["screen_name"]);
-        });
+        .then((data, response) => {
+            console.log('Logged in to Twitter successfully');
+            state.user = data;
+            return state;
+        })
+        .catch(error => Promise.reject(error));
 }
 
-let twitterFriends = null;
-function loadTwitterFriends() {
-    return twitter.followersAsync('list', null, process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET)
-    .then(function (data, response) {
-        console.log(data.users.filter(user => user.following).length);
-    });
+function loadTwitterFriends(state) {
+    console.log('Fetching all Twitter friends...');
+    return twitter.followersAsync('list', { count: 100 }, process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET)
+        .then((data, response) => {
+            state.friends = data.users.filter(user => user.following);
+            console.log('Fetched all', state.friends.length, 'Twitter friends');
+            return state;
+        })
+        .catch(error => Promise.reject(error));
 }
 
-function generateValentines() {
-    if (!baseBeaverImage) {
-        console.error('beaver doesn\'t exist for some reason');
-        return;
+function generateValentines(state) {
+    console.log('Generating valentines...');
+    return Promise.all(state.friends.map(user => generateValentine(user, state)))
+        .then(() => state);
+}
+
+function generateValentine(user, state) {
+    if (!state.baseBeaverImage) {
+        return Promise.reject('beaver doesn\'t exist for some reason');
     }
-    jimp.loadFont(jimp.FONT_SANS_32_BLACK)
-        .then(function (font) {
-            const beaver = baseBeaverImage.clone();
-            beaver.print(font, 105, 240, name).write(path.resolve(valentineDir, name + '.jpg'));
+    const name = (user['screen_name'] || 'my friend').toLowerCase();
+    const filePath = getFilePathForUser(name);
+    return jimp.loadFont(jimp.FONT_SANS_32_BLACK)
+        .then((font) => {
+            const beaver = state.baseBeaverImage.clone();
+            beaver.print(font, 105, 240, name)
+                .write(filePath);
+            console.log('Generated valentine for', user['screen_name']);
         });
 }
 
-function sendValentines() {
+function getFilePathForUser(user) {
+    let name;
+    if (typeof user === 'object' && user['screen_name']) {
+        name = user['screen_name'];
+    } else {
+        name = user;
+    }
+    return path.resolve(valentineDir, name + '.jpg')
+}
 
+function sendValentines(state) {
+    console.log('Sending valentines...');
+    return Promise.all(state.friends.map(user => sendValentine(user, state)))
+        .then(() => state)
+        .catch(error => Promise.reject(error));
+}
+
+function sendValentine(user, state) {
+    console.log('Sending valentine to', user['screen_name']);
+    return twitter.uploadMediaAsync({ media: getFilePathForUser(user['screen_name']) }, process.env.ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET)
+        .then((response) => {
+            const mediaId = response['media_id'];
+            if (!mediaId) {
+                return Promise.reject();
+            }
+            return twitter.statusesAsync(
+                'update',
+                { status: `@${user['screen_name']} Happy Valentine's Day ya goof`, 'media_ids': mediaId },
+                process.env.ACCESS_TOKEN,
+                process.env.ACCESS_TOKEN_SECRET
+            );
+        })
+        .catch(error => Promise.reject(error));
 }
 
 (function main() {
-    // // step 1 - load the picture
-    // loadValentine()
-    // // step 2 - load the friends
-    // .then(loginToTwitter)
-    // .then(loadTwitterFriends)
-    // // step 3 - create the valentines from the friends list and the picture
-    // .then(generateValentines)
-    // // step 4 - send out the valentines
-    // .then(sendValentines);
-    loginToTwitter()
-    .then(loadTwitterFriends)
-        .then(() => console.log('done'))
-        .then(() => process.exit(0));
+    // step 1 - load the picture
+    loadValentine({})
+        // step 2 - load the friends
+        .then(loginToTwitter)
+        .then(loadTwitterFriends)
+        // step 3 - create the valentines from the friends list and the picture
+        .then(generateValentines)
+        // step 4 - send out the valentines
+        .then(sendValentines)
+        .then(() => console.log('done :)'))
+        .then(() => process.exit(0))
+        .catch((reason) => {
+            console.error(reason);
+            process.exit(1);
+        });
 })();
